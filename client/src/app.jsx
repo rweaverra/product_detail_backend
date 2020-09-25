c440f9afb9d8f4670547e62f6024801adff7c967
Alison Sipos <alison.sipos@icloud.com>
2020-09-25 10:57:02 -0600

    Added alt text to images for accessibility

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-// import Container from 'react-bootstrap/Container'
-// import Jumbotron from 'react-bootstrap/Jumbotron'
-// import Button from 'react-bootstrap/Button';
-// import Col from 'react-bootstrap/Col';
-// import Row from 'react-bootstrap/Row';
-import { Container, Jumbotron, Button, Col, Row, Carousel } from 'react-bootstrap';
-import MainImage from './mainImage';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Carousel from 'react-bootstrap/Carousel';
+// import MainImage from './mainImage';
 
 // const apiTest = () => {
 //   fetch('http://52.26.193.201:3000/products/list')
@@ -19,7 +19,7 @@ import MainImage from './mainImage';
 
 const App = () => (
   <Container>
-    <br></br>
+    <br />
     <Jumbotron>
       <h1>The Proto Company</h1>
       <p>
@@ -38,26 +38,27 @@ const App = () => (
       </Row>
       <Row>
         <Col xs={0} s={2}> </Col>
-        <Col xs={12} s={6}> 
-        <Carousel interval={null}>
-          <Carousel.Item>
-            <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png"/>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png"/>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png"/>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png"/>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png"/>
-          </Carousel.Item>
-        </Carousel></Col>
-        <Col xs={12} s={4}> 
-          Product Details Here :D 
+        <Col xs={12} s={6}>
+          <Carousel interval={null}>
+            <Carousel.Item>
+              <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" />
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+        <Col xs={12} s={4}>
+          Product Details Here :D
         </Col>
         <Col xs={0} s={2}> </Col>
       </Row>

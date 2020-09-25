329117aee63d3a915676e17342cf1ceda309039b
Alison Sipos <alison.sipos@icloud.com>
2020-09-25 13:47:40 -0600

    Created tests for new components.

--- __tests__/test.spec.js
+++ __tests__/test.spec.js
@@ -1,26 +1,61 @@
-import App from '../client/src/app.jsx';
 import React from 'react';
 import { mount } from 'enzyme';
 import renderer from 'react-test-renderer';
-import { Container, Jumbotron } from 'react-bootstrap';
-
-const wrapper = mount(<App />);
+import { Carousel, Container, Jumbotron } from 'react-bootstrap';
+import App from '../client/src/app.jsx';
+import TitleImage from '../client/src/components/titleImage';
+import MainImage from '../client/src/components/mainImage';
 
 describe('App', () => {
-  test('simple react component test', () => {
-    expect(wrapper.find('h1').text()).toEqual('The Proto Company');
-  })
-  test('snapshot renders', () => {
-    const component = renderer.create(<App />);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+  // test('renders snapshot', () => {
+  //   const component = renderer.create(<App />);
+  //   let tree = component.toJSON();
+  //   expect(tree).toMatchSnapshot();
+  // });
   it('renders the inner Containers', () => {
     const wrapper = mount(<App />);
     expect(wrapper.find(Container).length).toEqual(2);
   });
-  it('renders the Jumbotron', () => {
+  it('renders the Title Image', () => {
     const wrapper = mount(<App />);
+    expect(wrapper.find(TitleImage).length).toEqual(1);
+  });
+  it('renders the Main Image', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(MainImage).length).toEqual(1);
+  });
+});
+
+describe('TitleImage', () => {
+  test('renders the Title Image', () => {
+    const wrapper = mount(<TitleImage />);
     expect(wrapper.find(Jumbotron).length).toEqual(1);
   });
+  it('renders the company name', () => {
+    const wrapper = mount(<TitleImage />);
+    expect(wrapper.find('h1').text()).toEqual('The Proto Company');
+  });
+  it('renders the informational text', () => {
+    const wrapper = mount(<TitleImage />);
+    expect(wrapper.find('p').text()).not.toBe('');
+  });
+});
+
+describe('MainImage', () => {
+  test('renders the main Carousel container', () => {
+    const wrapper = mount(<MainImage />);
+    expect(wrapper.find(Carousel).length).toEqual(1);
+  });
+  it('renders Carousel Items', () => {
+    const wrapper = mount(<MainImage />);
+    expect(wrapper.find(Carousel.Item).length).toEqual(5);
+  });
+  // it('sets image source correctly', () => {
+  //   const wrapper = mount(<MainImage />);
+  //   expect(wrapper.find('src').length).toEqual(5);
+  // });
+  // it('renders alternative text on images for accessibility', () => {
+  //   const wrapper = mount(<MainImage />);
+  //   expect(wrapper.find('alt').length).toEqual(5);
+  // });
 });

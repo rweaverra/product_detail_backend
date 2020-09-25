7f4b4b3b1d89637a45a5f69b9eac6e102f8c30aa
Alison Sipos <alison.sipos@icloud.com>
2020-09-25 11:07:31 -0600

    Added alt text to image and extra line at end of file.

--- client/src/mainImage.jsx
+++ client/src/mainImage.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 const MainImage = () => (
   <Carousel.Item>
-    <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png"/>
+    <img alt="placeholder" src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" />
   </Carousel.Item>
-)
+);
 
 export default MainImage;

b880e46ee1f4d8ff7a2ecee7449571e787c6e916
Alison Sipos <alison.sipos@icloud.com>
2020-09-25 13:51:12 -0600

    Created new components

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,69 +1,30 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Carousel from 'react-bootstrap/Carousel';
-// import MainImage from './mainImage';
+import { Container, Row } from 'react-bootstrap';
+import TitleImage from './components/titleImage';
+import MainImage from './components/mainImage';
 
-// const apiTest = () => {
-//   fetch('http://52.26.193.201:3000/products/list')
-//     .then(data => {
-//       console.log(data);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//         })
-// }
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
 
-const App = () => (
-  <Container>
-    <br />
-    <Jumbotron>
-      <h1>The Proto Company</h1>
-      <p>
-        This is a simple hero unit, a simple jumbotron-style component for calling
-        extra attention to featured content or information.
-      </p>
-      <p>
-        <Button variant="primary">Learn More</Button>
-      </p>
-    </Jumbotron>
-    <Container>
-      <Row>
-        <Col xs={0} s={2}> </Col>
-        <Col xs={12} s={8}> </Col>
-        <Col xs={0} s={2}> </Col>
-      </Row>
-      <Row>
-        <Col xs={0} s={2}> </Col>
-        <Col xs={12} s={6}>
-          <Carousel interval={null}>
-            <Carousel.Item>
-              <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" />
-            </Carousel.Item>
-          </Carousel>
-        </Col>
-        <Col xs={12} s={4}>
-          Product Details Here :D
-        </Col>
-        <Col xs={0} s={2}> </Col>
-      </Row>
-    </Container>
-  </Container>
-);
+    };
+  }
+
+  render() {
+    return (
+      <Container>
+        <br />
+        <TitleImage />
+        <Container>
+          <Row>
+            <MainImage />
+            Test
+          </Row>
+        </Container>
+      </Container>
+    );
+  }
+}
 
 export default App;

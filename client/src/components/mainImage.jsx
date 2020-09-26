6d1586ea5aaf9cccc36f095b35248b80af3f128b
Alison Sipos <alison.sipos@icloud.com>
2020-09-26 14:02:42 -0600

    Bug Fixed: imported node module css into main css file.

--- client/src/components/mainImage.jsx
+++ client/src/components/mainImage.jsx
@@ -1,8 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-// import ImageZoom from 'react-medium-image-zoom';
 import ControlledZoom from 'react-medium-image-zoom';
-// import 'react-medium-image-zoom/dist/styles.css';
 
 const MainImage = ({ styles }) => {
   const photoArr = styles.results && styles.results[0].photos;
@@ -13,6 +11,7 @@ const MainImage = ({ styles }) => {
   const handleZoomChange = useCallback((shouldZoom) => {
     setIsZoomed(shouldZoom);
   }, []);
+
   return (
     <Carousel interval={null} style={{ width: 600, height: 500 }}>
       {!!photoArr && photoArr.map((photo) => (

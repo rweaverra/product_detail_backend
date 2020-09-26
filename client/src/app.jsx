91c3fab122c862a1766d8ea5c0f5a3a7d064a60f
Alison Sipos <alison.sipos@icloud.com>
2020-09-25 19:09:17 -0600

    Refactored App to be a functional component. Using hooks to pass state to children components through props.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,57 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import TitleImage from './components/titleImage';
 import MainImage from './components/mainImage';
-// import listProducts from './lib/routes.js';
+import { getAllStyles } from './lib/routes';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const App = () => {
+  const [styles, setStyles] = useState({});
+  const [productId, setId] = useState(1);
 
-    };
-    // this.getProducts = this.getProducts.bind(this);
-  }
-  // getProducts((error, data) => {
-  //   if (error) {
-  //     throw(error)
-  //   } else {
-  //     console.log(data.results);
-  //   }
-  // })
+  useEffect(() => {
+    console.log('this time!');
+    getAllStyles((error, response) => {
+      console.log('data within get all styles', response.data);
+      if (error) {
+        return 'Could not get styles';
+      }
+      setId(response.product_id);
+      return setStyles(response);
+    });
+  }, []);
 
-  render() {
-    return (
+  return (
+    <Container>
+      <br />
+      <TitleImage />
       <Container>
-        <br />
-        <TitleImage />
-        <Container>
-          <Row>
-            <MainImage />
-            Stars will go here
-            <br />
-            Category will go here
-            <br />
-            Product Name will go here
-            <br />
-            Price will go here
-            <br />
-            Style and Selected Style will go here
-            <br />
-            Style Thumbnails will go here
-            <br />
-            Size and Quantity Dropdowns will go here
-            <br />
-            Add to Cart and Favorite Buttons will go here
-            <br />
-          </Row>
-        </Container>
-        <Container>
-          Product Description will go here
-        </Container>
+        <Row>
+          <MainImage styles={styles} />
+          Stars will go here
+          <br />
+          Category will go here
+          <br />
+          Product Name will go here
+          <br />
+          Price will go here
+          <br />
+          Style and Selected Style will go here
+          <br />
+          Style Thumbnails will go here
+          <br />
+          Size and Quantity Dropdowns will go here
+          <br />
+          Add to Cart and Favorite Buttons will go here
+          <br />
+        </Row>
       </Container>
-    );
-  }
-}
+      <Container>
+        Product Description will go here
+      </Container>
+    </Container>
+  );
+};
 
 export default App;

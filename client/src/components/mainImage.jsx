28b1701ec716345e627a37a6e479dcf222b0352c
Alison Sipos <alison.sipos@icloud.com>
2020-09-25 19:10:02 -0600

    Refactored to take in props to render api data.

--- client/src/components/mainImage.jsx
+++ client/src/components/mainImage.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-// import { useState } from 'react';
 
-const MainImage = () => (
-  <Carousel interval={null} style={{ width: 600, height: 400 }}>
-    <Carousel.Item>
-      <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" width="auto" height="400" />
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" width="auto" height="400" />
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" width="auto" height="400" />
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" width="auto" height="400" />
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" alt="placeholder" width="auto" height="400" />
-    </Carousel.Item>
-  </Carousel>
-);
+const MainImage = ({ styles }) => {
+  const photoArr = styles.results && styles.results[0].photos;
+  return (
+    <Carousel interval={null} style={{ width: 600, height: 500 }}>
+      {!!photoArr && photoArr.map((photo) => (
+        <Carousel.Item key={photo.url}>
+          <img
+            src={photo.url}
+            alt="current style"
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
 
 export default MainImage;

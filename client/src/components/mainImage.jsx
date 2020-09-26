a347d8c245e62b992c27bf762d05827bed5f6ee4
Alison Sipos <alison.sipos@icloud.com>
2020-09-26 13:35:00 -0600

    Tried correcting bug using controlled zoom. bug persists.

--- client/src/components/mainImage.jsx
+++ client/src/components/mainImage.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 // import ImageZoom from 'react-medium-image-zoom';
-import Zoom from 'react-medium-image-zoom';
+import ControlledZoom from 'react-medium-image-zoom';
 // import 'react-medium-image-zoom/dist/styles.css';
 
 const MainImage = ({ styles }) => {
   const photoArr = styles.results && styles.results[0].photos;
+  const [isZoomed, setIsZoomed] = useState(false);
+  const handleImgLoad = useCallback(() => {
+    setIsZoomed(true);
+  }, []);
+  const handleZoomChange = useCallback(shouldZoom => {
+    setIsZoomed(shouldZoom);
+  }, []);
   return (
     <Carousel interval={null} style={{ width: 600, height: 500 }}>
       {!!photoArr && photoArr.map((photo) => (
         <Carousel.Item key={photo.url} id="mainImage">
-          <Zoom>
+          <ControlledZoom isZoomed={isZoomed} onZoomChange={handleZoomChange} closeText="Unzoom Image" openText="Zoom Image" zoomZindex="1">
             <img
               src={photo.url}
               alt={photo.name}
+              onLoad={handleImgLoad}
             />
-          </Zoom>
+          </ControlledZoom>
         </Carousel.Item>
       ))}
     </Carousel>

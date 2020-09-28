0ec0ca62ab5670cf1aef52aaa7701153a796626c
Alison Sipos <alison.sipos@icloud.com>
2020-09-28 14:43:33 -0600

    Features is now taking in products features from api.

--- client/src/components/features.jsx
+++ client/src/components/features.jsx
@@ -1,20 +1,43 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-const Features = ({ product }) => (
-  <Card className="features-height">
-    <Card.Header className="accordion-style">
-      <p className="accordion-links features-title">Features</p>
-    </Card.Header>
-    <Card.Body className="accordion-body">
-      List of features
-    </Card.Body>
-  </Card>
-);
+const Features = ({ product }) => {
+  const featureList = product && product.features;
+  const featureSet = new Set(featureList);
+  const featureArray = [...featureSet];
 
-// Features.defaultProps = {
-// };
-// Features.propTypes = {
-// };
+  return (
+    <Card className="features-style">
+      <Card.Header>
+        <p className="features-links">Features</p>
+      </Card.Header>
+      <Card.Body className="accordion-body">
+        {
+        !!featureArray && featureArray.map((trait) => (
+          <Row key={500 + trait.feature}>
+            <Col>
+              <img
+                src="https://icons-for-free.com/iconfiles/png/512/checkmark+circle+icon-1320195547843144574.png"
+                className="features-check"
+                alt="checkmark"
+              />
+              {
+              trait.value === 'null' ? trait.feature : (trait.value).concat(' ', trait.feature)
+              }
+            </Col>
+          </Row>
+        ))
+        }
+      </Card.Body>
+    </Card>
+  );
+};
+
+Features.defaultProps = {
+  product: PropTypes.objectOf(PropTypes.string),
+};
+Features.propTypes = {
+  product: PropTypes.objectOf(PropTypes.string, PropTypes.number),
+};
 export default Features;

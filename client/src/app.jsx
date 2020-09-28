7a622910cf9b6d6947870a7294b68b1eb594e504
Alison Sipos <alison.sipos@icloud.com>
2020-09-28 16:29:10 -0600

    Put components into Columns and Rows to allow for controlled alignment

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -7,6 +7,7 @@ import MainImage from './components/mainImage';
 import DetailedInfo from './components/detailedInfo';
 import Features from './components/features';
 import ProductTitle from './components/productTitle';
+import ProductCategory from './components/productCategory';
 import { getAllStyles, getInfo } from './lib/routes';
 
 const App = () => {
@@ -45,24 +46,21 @@ const App = () => {
       <TitleImage />
       <Container>
         <Row>
-          <Col xs={12} sm={8} md={8}>
+          <Col xs={12} sm={12} md={8} lg={8}>
             <MainImage styles={styles} />
           </Col>
-          <Col xs={12} sm={4} md={4}>
-            Stars will go here
-            <br />
-            Category will go here
-            <br />
-            <ProductTitle product={product} />
-            Price will go here
-            <br />
-            Style and Selected Style will go here
-            <br />
-            Style Thumbnails will go here
-            <br />
-            Size and Quantity Dropdowns will go here
-            <br />
-            Add to Cart and Favorite Buttons will go here
+          <Col xs={12} sm={12} md={4} lg={4}>
+            <Row>
+              <span role="img" aria-label="stars">⭐️⭐️⭐️⭐️⭐️</span>
+              Stars will go here
+            </Row>
+            <Row><ProductCategory product={product} /></Row>
+            <Row><ProductTitle product={product} /></Row>
+            <Row>Price will go here</Row>
+            <Row>Style and Selected Style will go here</Row>
+            <Row>Style Thumbnails will go here</Row>
+            <Row>Size and Quantity Dropdowns will go here</Row>
+            <Row>Add to Cart and Favorite Buttons will go here</Row>
           </Col>
         </Row>
       </Container>

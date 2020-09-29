209c801935a49fcd2d2f7d74efd6fbfc009b57d6
Alison Sipos <alison.sipos@icloud.com>
2020-09-29 17:46:30 -0600

    Played around with stars and passing props, will need to redo.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,43 +1,89 @@
+/* eslint-disable no-console */
 import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import TitleImage from './components/titleImage';
 import ProductInfo from './components/productInfo';
 import ProductDetails from './components/productDetails';
-import { getAllStyles, getInfo } from './lib/routes';
+import { getAllStyles, getInfo, getReviews } from './lib/routes';
 
 const App = () => {
   const [styles, setStyles] = useState({});
   const [productId, setId] = useState(1);
   const [product, setProduct] = useState({});
+  const [reviews, setReviews] = useState({});
+  // const [rating, getRating] = useState(0);
+  // const [count, getCount] = useState(0);
+
+  // const getTheAverageRating = (starData) => {
+  //   console.log('App: star data ', starData);
+  //   let reviewCount = 0;
+  //   let reviewTotal = 0;
+  //   const ratingArr = Object.entries(starData);
+  //   for (let i = 0; i < ratingArr.length; i += 1) {
+  //     const current = ratingArr[i];
+  //     const stars = Number(current[0]);
+  //     const counter = current[1];
+  //     reviewCount += counter;
+  //     reviewTotal += stars * counter;
+  //   }
+  //   console.log('App: logging total and count', reviewTotal, reviewCount);
+  //   return reviewTotal / reviewCount;
+  // };
+  // const getTheReviewCount = (starData) => {
+  //   let reviewCount = 0;
+  //   const ratingArr = Object.entries(starData);
+  //   for (let i = 0; i < ratingArr.length; i += 1) {
+  //     const current = ratingArr[i];
+  //     const counter = current[1];
+  //     reviewCount += counter;
+  //   }
+  //   return reviewCount;
+  // };
 
   useEffect(() => {
     getAllStyles((error, response) => {
-      // console.log('data within get all styles', response.data);
       if (error) {
         return 'Could not get styles';
       }
       return setStyles(response);
     });
-    // listProducts((error, response) => {
-    //   if (error) {
-    //     return 'Could not get products';
-    //   }
-    //   return setProducts(response);
-    // });
     getInfo((error, response) => {
       if (error) {
         return 'Could not get products';
       }
-      // console.log('id in app: ', response.id);
       setId(response.id);
       return setProduct(response);
     });
+    getReviews((error, response) => {
+      if (error) {
+        return 'Could not get reviews';
+      }
+      return setReviews(response.ratings); // object
+    });
+    // getRating((error, response) => {
+    //   if (error) {
+    //     return 'Could not get ratings';
+    //   }
+    //   console.log('App: response from getRating', response);
+    //   const average = getTheAverageRating(response);
+    //   return getRating(average);
+    // });
+    // getCount((error, response) => {
+    //   if (error) {
+    //     return 'Could not get review count';
+    //   }
+    //   console.log('App: response from getCount', response);
+    //   const numberOReviews = getTheReviewCount(response);
+    //   return getCount(numberOReviews);
+    // });
   }, []);
 
+  console.log('reviews before passing to product info: ', reviews);
   return (
     <Container>
       <TitleImage />
       <ProductInfo styles={styles} product={product} />
+      {/* <ProductInfo styles={styles} product={product} rating={rating} count={count} /> */}
       <ProductDetails product={product} productId={productId} />
       <br />
     </Container>
--- client/src/components/productInfo.jsx
+++ client/src/components/productInfo.jsx
@@ -1,52 +1,62 @@
+/* eslint-disable no-console */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MainImage from './mainImage';
+// import StarRating from './starRating';
 // import ProductTitle from './productTitle';
 // import ProductCategory from './productCategory';
 
-const ProductInfo = ({ styles, product }) => (
-  <Container>
-    <Row>
-      {/* Product Image Carousel & Thumbnail Viewer */}
-      <Col xs={12} sm={12} md={8} lg={8}>
-        <MainImage styles={styles} />
-      </Col>
-      <Col xs={12} sm={12} md={4} lg={4}>
-        {/* Product Star Ratings */}
-        <Row>
-          <span role="img" aria-label="stars">⭐️⭐️⭐️⭐️⭐️</span>
-          Stars will go here
-        </Row>
-        {/* Product Category */}
-        <Row>
-          <div className="product-category">
-            category:&nbsp;
-            { product.category }
-          </div>
-        </Row>
-        {/* Product Title */}
-        <Row>
-          <div className="product-title">
-            { product.name }
-          </div>
-        </Row>
-        {/* Product Price */}
-        <Row>Price will go here</Row>
-        {/* Product Style */}
-        <Row>Style and Selected Style will go here</Row>
-        {/* Product Style Thumbnails */}
-        <Row>Style Thumbnails will go here</Row>
-        {/* Size and Quantity Selectors */}
-        <Row>Size and Quantity Dropdowns will go here</Row>
-        {/* Cart Options */}
-        <Row>Add to Cart and Favorite Buttons will go here</Row>
-      </Col>
-    </Row>
-  </Container>
-);
+const ProductInfo = ({ styles, product }) => {
+  // console.log('count and rating in productInfo before passing to StarRating', count, rating);
+  console.log('ProductInfo: styles, product');
+  return (
+    <Container>
+      <Row>
+        {/* Product Image Carousel & Thumbnail Viewer */}
+        <div>
+          <Col xs={12}>
+            <MainImage styles={styles} />
+          </Col>
+        </div>
+        <div>
+          <Col xs={12}>
+            {/* Product Star Ratings */}
+            <Row>
+              Redo Star Ratings Here.
+              {/* <StarRating rating={rating} count={count} /> */}
+            </Row>
+            {/* Product Category */}
+            <Row>
+              <div className="product-category">
+                category:&nbsp;
+                { product.category }
+              </div>
+            </Row>
+            {/* Product Title */}
+            <Row>
+              <div className="product-title">
+                { product.name }
+              </div>
+            </Row>
+            {/* Product Price */}
+            <Row>Price will go here</Row>
+            {/* Product Style */}
+            <Row>Style and Selected Style will go here</Row>
+            {/* Product Style Thumbnails */}
+            <Row>Style Thumbnails will go here</Row>
+            {/* Size and Quantity Selectors */}
+            <Row>Size and Quantity Dropdowns will go here</Row>
+            {/* Cart Options */}
+            <Row>Add to Cart and Favorite Buttons will go here</Row>
+          </Col>
+        </div>
+      </Row>
+    </Container>
+  );
+};
 
 ProductInfo.propTypes = {
   styles: PropTypes.shape({
@@ -62,6 +72,14 @@ ProductInfo.propTypes = {
     default_price: PropTypes.string,
     features: PropTypes.arrayOf(PropTypes.object),
   }).isRequired,
+  // reviews: PropTypes.shape({
+  //   product_id: PropTypes.string,
+  //   ratings: PropTypes.objectOf(PropTypes.string),
+  //   recommended: PropTypes.objectOf(PropTypes.string),
+  //   characteristics: PropTypes.objectOf(PropTypes.string),
+  // }).isRequired,
+  // rating: PropTypes.number.isRequired,
+  // count: PropTypes.number.isRequired,
 };
 
 export default ProductInfo;

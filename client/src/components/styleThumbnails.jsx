f7e76ab81633b05665eef6d8d41c443cd47ef03c
Alison Sipos <alison.sipos@icloud.com>
2020-09-30 15:26:30 -0600

    Created basic formatting with placeholder images to begin playing with layout.

--- client/src/components/styleThumbnails.jsx
+++ client/src/components/styleThumbnails.jsx
@@ -5,3 +5,47 @@
 // What will the badge look like?
 // How will you grab the image url and the style id?
 // What will be held in state?
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+const StyleThumbnails = () => (
+  <Container>
+    {/* mapping fn returns the following */}
+    <Row>
+      <Col xs={4} md={2}>
+        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+      </Col>
+    </Row>
+    {/* end mapping fn */}
+    <Row>
+      <Col xs={4} md={2}>
+        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={4} md={2}>
+        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={4} md={2}>
+        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={4} md={2}>
+        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={4} md={2}>
+        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default StyleThumbnails;

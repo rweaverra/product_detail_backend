8970d82515e879379094f3153d2412c55c7015df
Alison Sipos <alison.sipos@icloud.com>
2020-09-30 15:15:19 -0600

    Created file for thumbnail image component that displays the varius styles, included comments.

---
+++ client/src/components/styleThumbnails.jsx
@@ -0,0 +1,7 @@
+// style thumbnails go here
+// How will you display the style images?
+// Will you use the thumbnail or the full image from the style?
+// How will a user see they selected a style?
+// What will the badge look like?
+// How will you grab the image url and the style id?
+// What will be held in state?

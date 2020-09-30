854a2265debca4425a6a2a230f48df9a97efe6fb
Alison Sipos <alison.sipos@icloud.com>
2020-09-30 15:52:53 -0600

    Adjusted component to render in a preferred format on the page.

--- client/src/components/styleThumbnails.jsx
+++ client/src/components/styleThumbnails.jsx
@@ -6,46 +6,29 @@
 // How will you grab the image url and the style id?
 // What will be held in state?
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 
 const StyleThumbnails = () => (
-  <Container>
-    {/* mapping fn returns the following */}
-    <Row>
-      <Col xs={4} md={2}>
-        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
-      </Col>
-    </Row>
-    {/* end mapping fn */}
-    <Row>
-      <Col xs={4} md={2}>
-        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={4} md={2}>
-        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={4} md={2}>
-        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={4} md={2}>
-        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={4} md={2}>
-        <Image src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
-      </Col>
-    </Row>
-  </Container>
+  <div className="thumbnails-cage">
+    <Col sm={10} md={7}>
+      {/* mapping fn returns the following */}
+      <Image className="thumbnails-img" src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+      {/* end mapping fn */}
+      <Image className="thumbnails-img" src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+
+      <Image className="thumbnails-img" src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+
+      <Image className="thumbnails-img" src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+
+      <Image className="thumbnails-img" src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+
+      <Image className="thumbnails-img" src="https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png" roundedCircle />
+    </Col>
+
+  </div>
 );
 
 export default StyleThumbnails;

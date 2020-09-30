246dd1a99ee15a6840c2aeb77dd7e1adeb384809
Alison Sipos <alison.sipos@icloud.com>
2020-09-30 16:43:01 -0600

    Cleaned up comments and unneeded code.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -11,34 +11,6 @@ const App = () => {
   const [productId, setId] = useState(1);
   const [product, setProduct] = useState({});
   const [reviews, setReviews] = useState({});
-  // const [rating, getRating] = useState(0);
-  // const [count, getCount] = useState(0);
-
-  // const getTheAverageRating = (starData) => {
-  //   console.log('App: star data ', starData);
-  //   let reviewCount = 0;
-  //   let reviewTotal = 0;
-  //   const ratingArr = Object.entries(starData);
-  //   for (let i = 0; i < ratingArr.length; i += 1) {
-  //     const current = ratingArr[i];
-  //     const stars = Number(current[0]);
-  //     const counter = current[1];
-  //     reviewCount += counter;
-  //     reviewTotal += stars * counter;
-  //   }
-  //   console.log('App: logging total and count', reviewTotal, reviewCount);
-  //   return reviewTotal / reviewCount;
-  // };
-  // const getTheReviewCount = (starData) => {
-  //   let reviewCount = 0;
-  //   const ratingArr = Object.entries(starData);
-  //   for (let i = 0; i < ratingArr.length; i += 1) {
-  //     const current = ratingArr[i];
-  //     const counter = current[1];
-  //     reviewCount += counter;
-  //   }
-  //   return reviewCount;
-  // };
 
   useEffect(() => {
     getAllStyles((error, response) => {
@@ -61,26 +33,8 @@ const App = () => {
       console.log('App: get reviews response', response.ratings);
       return setReviews(response.ratings); // object
     });
-    // getRating((error, response) => {
-    //   if (error) {
-    //     return 'Could not get ratings';
-    //   }
-    //   console.log('App: response from getRating', response);
-    //   const average = getTheAverageRating(response);
-    //   return getRating(average);
-    // });
-    // getCount((error, response) => {
-    //   if (error) {
-    //     return 'Could not get review count';
-    //   }
-    //   console.log('App: response from getCount', response);
-    //   const numberOReviews = getTheReviewCount(response);
-    //   return getCount(numberOReviews);
-    // });
   }, []);
 
-  // console.log('App: rating before passing to product info: ', rating);
-  // console.log('App: count before passing to product info: ', count);
   if (Object.keys(reviews).length === 0
   || Object.keys(product).length === 0
   || Object.keys(styles).length === 0) {
@@ -93,9 +47,7 @@ const App = () => {
     <Container>
       <TitleImage />
       <ProductInfo styles={styles} product={product} reviews={reviews} />
-      {/* <ProductInfo styles={styles} product={product} rating={rating} count={count} /> */}
       <ProductDetails product={product} productId={productId} />
-      <br />
     </Container>
   );
 };

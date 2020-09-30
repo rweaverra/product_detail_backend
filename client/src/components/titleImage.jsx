d9a1b0a6683397bcd6f4aa886058beed27743502
Alison Sipos <alison.sipos@icloud.com>
2020-09-30 16:52:16 -0600

    Removed unnecessary break between h1 an p elements. If more spacing is needed I will adjust the jumbotron-h1 class.

--- client/src/components/titleImage.jsx
+++ client/src/components/titleImage.jsx
@@ -5,7 +5,6 @@ import Button from 'react-bootstrap/Button';
 const TitleImage = () => (
   <Jumbotron>
     <h1 className="jumbotron-h1">The Proto Company</h1>
-    <br />
     <p className="jumbotron-p">
       Add a touch of comfort to your everyday wardrobe with our casual wear,
       including soft sweaters and hoodies, joggers, cargo pants and relaxed

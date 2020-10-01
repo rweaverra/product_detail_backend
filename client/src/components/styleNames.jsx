c5dd7f61cb8c1f72365b8ea0b0a7406d3e066d90
Alison Sipos <alison.sipos@icloud.com>
2020-10-01 15:10:24 -0600

    Created component that will render the currently selected style.

--- client/src/components/styleNames.jsx
+++ client/src/components/styleNames.jsx
@@ -1,3 +1,25 @@
 // Style and Selected Style
 // How will you display the style name?
 // How will you label the style so users know it's the style?
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const StyleName = ({ styles }) => (
+  <div>
+    <h3 className="styles-name">
+      Style →&nbsp;
+    </h3>
+    <h3 className="styles-name-selected">
+      {styles.results[0].name}
+    </h3>
+  </div>
+);
+
+StyleName.propTypes = {
+  styles: PropTypes.shape({
+    product_id: PropTypes.string,
+    results: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+};
+
+export default StyleName;

3fbca3a6e664f08af7a27425223c5527adb767b7
Alison Sipos <alison.sipos@icloud.com>
2020-10-01 15:07:37 -0600

    Created dropdown component to select quantity, exported.

--- client/src/components/quantityDropdown.jsx
+++ client/src/components/quantityDropdown.jsx
@@ -2,3 +2,21 @@
 // how many items of the current style are left
 // display quantity of 5 to start
 // display max quantity amount once dropdown functionality is understood
+import React from 'react';
+import Dropdown from 'react-dropdown';
+
+const QuantityDropdown = () => {
+  const options = [
+    'one', 'two', 'three', 'four', 'five',
+  ];
+  return (
+    <Dropdown
+      options={options}
+      // onChange={}
+      value="quantity"
+      placeholder="Select an option"
+    />
+  );
+};
+
+export default QuantityDropdown;

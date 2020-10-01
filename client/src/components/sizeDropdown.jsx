52f92a3bd0d35d9a59edfa6d61cc40e8567a5d45
Alison Sipos <alison.sipos@icloud.com>
2020-10-01 15:09:48 -0600

    Created dropdown component to select size, exported.

--- client/src/components/sizeDropdown.jsx
+++ client/src/components/sizeDropdown.jsx
@@ -1,3 +1,21 @@
 // The dropdown for size will go here
 // How many items are available for each style and size?
 // if no items are available how will you display that?
+import React from 'react';
+import Dropdown from 'react-dropdown';
+
+const SizeDropdown = () => {
+  const options = [
+    'x-small', 'small', 'medium', 'large', 'x-large',
+  ];
+  return (
+    <Dropdown
+      options={options}
+      // onChange={}
+      value="size"
+      placeholder="Select an option"
+    />
+  );
+};
+
+export default SizeDropdown;

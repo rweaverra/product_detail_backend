6348e8ff769a7eb128840fa6d9dfcc60e8f400ea
Alison Sipos <alison.sipos@icloud.com>
2020-10-01 16:23:24 -0600

    Began templating shopping cart.

---
+++ client/src/components/shoppingCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = () => (
+  <div>
+    <img
+      alt="shopping cart"
+      className="shopping-bag-root"
+      src="https://image.flaticon.com/icons/png/512/126/126083.png"
+    />
+  </div>
+);
+export default Cart;

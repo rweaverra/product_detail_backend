9844747803ec9014178d5a45ab5fb6d116336d4f
Alison Sipos <alison.sipos@icloud.com>
2020-10-03 17:43:41 -0600

    Removed Accordion functionality as it no longer fit the aesthetic of the page.

--- client/src/components/productDetails.jsx
+++ client/src/components/productDetails.jsx
@@ -2,20 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Accordion from 'react-bootstrap/Accordion';
-import Col from 'react-bootstrap/Col';
 import DetailedInfo from './detailedInfo';
-import Features from './features';
 
 const ProductDetails = ({ product, productId }) => (
   <Container>
     <Row>
-      <Col xs={12} sm={8} md={8}>
-        <Accordion className="accordion-height"><DetailedInfo product={product} productId={productId} /></Accordion>
-      </Col>
-      <Col xs={12} sm={4} md={4}>
-        <Features product={product} />
-      </Col>
+      <DetailedInfo product={product} productId={productId} />
     </Row>
   </Container>
 );

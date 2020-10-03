6211d064f201b0a4f87f2a90e53a2ffcf13ace30
Alison Sipos <alison.sipos@icloud.com>
2020-10-03 17:40:58 -0600

    Restructured features into detailedInfo component

--- client/src/components/detailedInfo.jsx
+++ client/src/components/detailedInfo.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
-import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import PropTypes from 'prop-types';
+import Col from 'react-bootstrap/Col';
+import { v4 as uuidv4 } from 'uuid';
 
 const DetailedInfo = ({ product }) => {
   const slogan = product && product.slogan;
   const description = product && product.description;
+  const featureList = product && product.features;
+  const featureSet = new Set(featureList);
+  const featureArray = [...featureSet];
   return (
-    <Card className="accordion-style">
-      <Card.Header>
-        <Accordion.Toggle as={Button} variant="link" eventKey="0" className="accordion-links">
-          <Row>
+    <Container>
+      <Card.Header className="details-header">
+        <Row>
+          <Col xs={8}>
             { slogan }
-          </Row>
-          <Row>
-            <small>click for more info </small>
-          </Row>
-        </Accordion.Toggle>
+          </Col>
+          <Col xs={4}>
+            Features:
+          </Col>
+        </Row>
       </Card.Header>
-      <Accordion.Collapse eventKey="0" className="accordion-body">
-        <Card.Body>
-          <Row>
+      <Card.Body className="details-body">
+        <Row>
+          <Col xs={8} className="details-body-divider">
             { description }
-          </Row>
-        </Card.Body>
-      </Accordion.Collapse>
-    </Card>
+          </Col>
+          <Col xs={4}>
+            {
+        !!featureArray && featureArray.map((trait) => (
+          <div key={uuidv4()}>
+            <img
+              src="https://icons-for-free.com/iconfiles/png/512/checkmark+circle+icon-1320195547843144574.png"
+              className="features-check"
+              alt="checkmark"
+            />
+            {
+              trait.value === 'null' ? trait.feature : (trait.value).concat(' ', trait.feature)
+              }
+          </div>
+        ))
+        }
+          </Col>
+        </Row>
+      </Card.Body>
+    </Container>
   );
 };
 
--- client/src/components/socialShare.jsx
+++ client/src/components/socialShare.jsx
@@ -11,47 +11,61 @@ import {
 } from 'react-share';
 import PropTypes from 'prop-types';
 
-const SocialShare = ({ styles, product }) => (
-  <div>
-    <TwitterShareButton
-      className="social-spacing"
-      title={product.name}
-      via="TheProtoCompany"
-      hashtags={['TheProtoCo', 'MyProto']}
-      url="twitter.com"
-    >
-      <TwitterIcon size={24} round />
-    </TwitterShareButton>
-    <FacebookShareButton
-      className="social-spacing"
-      quote="Check out this style from The Proto Company!"
-      hashtag="TheProtoCo"
-      url="facebook.com"
-    >
-      <FacebookIcon size={24} round />
-    </FacebookShareButton>
-    <PinterestShareButton
-      className="social-spacing"
-      media={styles.results}
-      description={product.name}
-      url="pinterest.com"
-    >
-      <PinterestIcon size={24} round />
-    </PinterestShareButton>
-    <EmailShareButton
-      className="social-spacing"
-      subject={product.name}
-      body="Check out this style I found at the Proto Company!"
-    >
-      <EmailIcon size={24} round />
-    </EmailShareButton>
-  </div>
-);
+const SocialShare = ({ product, currentStyle, styles }) => {
+  const body = `Check out ${product.name} in ${currentStyle.name} from The Proto Company!`;
+  const subject = `${product.name} in ${currentStyle.name} from The Proto Co.`;
+  return (
+    <div>
+      <TwitterShareButton
+        className="social-spacing"
+        title={body}
+        via="TheProtoCompany"
+        hashtags={['TheProtoCo', 'MyProto']}
+        url="protoco.com"
+      >
+        <TwitterIcon size={24} round />
+      </TwitterShareButton>
+      <FacebookShareButton
+        className="social-spacing"
+        quote={body}
+        hashtag="TheProtoCo"
+        url={styles.results[0].photos[0].url}
+      >
+        <FacebookIcon size={24} round />
+      </FacebookShareButton>
+      <PinterestShareButton
+        className="social-spacing"
+        media={styles.results[0].photos[0].url}
+        description={body}
+        url="protoco.com"
+      >
+        <PinterestIcon size={24} round />
+      </PinterestShareButton>
+      <EmailShareButton
+        className="social-spacing"
+        subject={subject}
+        body={body}
+        url={styles.results[0].photos[0].url}
+      >
+        <EmailIcon size={24} round />
+      </EmailShareButton>
+    </div>
+  );
+};
 
 SocialShare.propTypes = {
   styles: PropTypes.shape({
     product_id: PropTypes.string,
     results: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+  currentStyle: PropTypes.shape({
+    style_id: PropTypes.number,
+    name: PropTypes.string,
+    original_price: PropTypes.string,
+    sale_price: PropTypes.string,
+    default: PropTypes.number,
+    photos: PropTypes.arrayOf(PropTypes.object),
+    skus: PropTypes.objectOf(PropTypes.number),
   }).isRequired,
   product: PropTypes.shape({
     id: PropTypes.number,

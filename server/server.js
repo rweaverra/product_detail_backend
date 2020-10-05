06faa6fe336b255d9343b42fd494e1c853470f19
Alison Sipos <alison.sipos@icloud.com>
2020-10-05 16:57:25 -0600

    Created proxy server.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 
 const app = express();
-const PORT = 8080;
+const PORT = 3030;
 
 app.use(express.static('../client/dist'));
-
-app.get('/:id', (req,res) => {
-
-});
 
 app.listen(PORT, () => {
   console.log(`Server running and listening on port: ${PORT}`);

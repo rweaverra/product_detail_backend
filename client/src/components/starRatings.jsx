ac98b993af940e0d3cd6c0164919ae56ddaf91e4
Alison Sipos <alison.sipos@icloud.com>
2020-10-06 15:59:14 -0600

    Added numerical rating

--- client/src/components/starRatings.jsx
+++ client/src/components/starRatings.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import StarRatings from 'react-star-ratings';
+import Row from 'react-bootstrap/Row';
 
 const StarRating = ({ rating, count }) => (
   <div>
-    <StarRatings
-      rating={rating}
-      starRatedColor="#F5B895"
-      starEmptyColor="#B5C7D3"
-      numberOfStars={5}
-      starDimension="25px"
-      starSpacing="1px"
-      name="rating"
-    />
+    <Row>
+      <div className="rating-num">{rating.toFixed(2)}</div>
+      <StarRatings
+        rating={rating}
+        starRatedColor="#F5B895"
+        starEmptyColor="#B5C7D3"
+        numberOfStars={5}
+        starDimension="25px"
+        starSpacing="1px"
+        name="rating"
+      />
+    </Row>
     <div className="stars-review-link stars-spacing">
       <a href="https://github.com/callback-kids/ratings-and-reviews">
         View&nbsp;

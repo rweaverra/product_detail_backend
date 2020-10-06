c0bb2a561e375edb50857d72ec7d498772ff71ac
Alison Sipos <alison.sipos@icloud.com>
2020-10-06 16:24:00 -0600

    Dropdown now displaying sku sizing for currentStyle.

--- client/src/components/sizeDropdown.jsx
+++ client/src/components/sizeDropdown.jsx
@@ -1,13 +1,24 @@
+/* eslint-disable no-console */
 // The dropdown for size will go here
 // How many items are available for each style and size?
 // if no items are available how will you display that?
 import React from 'react';
 import Dropdown from 'react-dropdown';
+import PropTypes from 'prop-types';
 
-const SizeDropdown = () => {
-  const options = [
-    'x-small', 'small', 'medium', 'large', 'x-large',
-  ];
+const SizeDropdown = ({ currentStyle }) => {
+  console.log('SizeDropdown: current Style', currentStyle);
+  console.log('SizeDropdown: current Style skus', Object.keys(currentStyle.skus));
+  // const options = [
+  //   'x-small', 'small', 'medium', 'large', 'x-large',
+  // ];
+  let options = [];
+  if (
+    currentStyle.skus.length !== 0
+    || Object.keys(currentStyle.skus) !== null
+    || Object.keys(currentStyle.skus) !== undefined) {
+    options = Object.keys(currentStyle.skus);
+  }
   return (
     <Dropdown
       options={options}
@@ -16,6 +27,18 @@ const SizeDropdown = () => {
       placeholder="Select an option"
     />
   );
+};
+
+SizeDropdown.propTypes = {
+  currentStyle: PropTypes.shape({
+    'default?': PropTypes.number,
+    name: PropTypes.string,
+    original_price: PropTypes.string,
+    photos: PropTypes.arrayOf(PropTypes.object),
+    sale_price: PropTypes.string,
+    skus: PropTypes.objectOf(PropTypes.any),
+    style_id: PropTypes.number,
+  }).isRequired,
 };
 
 export default SizeDropdown;

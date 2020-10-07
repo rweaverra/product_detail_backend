e2a19bfaf5f23cafd9d852f037c05166e55c202c
Alison Sipos <alison.sipos@icloud.com>
2020-10-06 19:08:19 -0600

    Removed comments not needed.

--- client/src/components/quantityDropdown.jsx
+++ client/src/components/quantityDropdown.jsx
@@ -1,7 +1,3 @@
-// The dropdown for quantity selection will go here
-// how many items of the current style are left
-// display quantity of 5 to start
-// display max quantity amount once dropdown functionality is understood
 import React from 'react';
 import Dropdown from 'react-dropdown';
 import PropTypes from 'prop-types';

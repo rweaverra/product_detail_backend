e51eceda61752b4ef765304206ec7bba13392efb
Alison Sipos <alison.sipos@icloud.com>
2020-10-07 20:35:35 -0600

    Created a shopping cart table. Currently has dummy data.

--- client/src/components/shoppingCart.jsx
+++ client/src/components/shoppingCart.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
+// import Image from 'react-bootstrap/Image';
+import Table from 'react-bootstrap/Table';
 
 const Cart = () => (
   <div>
-    <img
-      alt="shopping cart"
-      className="shopping-bag-root"
-      src="https://image.flaticon.com/icons/png/512/126/126083.png"
-      // src="https://cdn.onlinewebfonts.com/svg/img_400462.png"
-      // src="https://www.pngkey.com/png/full/261-2611214_picture-transparent-supermarket-remove-shopping-icon-e-shopping.png"
-    />
+    <Table>
+      <thead className="cart-table-header">
+        <tr>
+          <th>Product Name</th>
+          <th>Product Style</th>
+          <th>Size</th>
+          <th>Quantity</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody className="cart-table-body">
+        <tr>
+          <td>Monicle</td>
+          <td>Gold</td>
+          <td>S</td>
+          <td>1</td>
+          <td>$100.00</td>
+        </tr>
+        <tr>
+          <td>Panda Onsie</td>
+          <td>Black & White</td>
+          <td>S</td>
+          <td>5</td>
+          <td>$320.00</td>
+        </tr>
+        <tr className="totals">
+          <td />
+          <td />
+          <td />
+          <td>6 Total</td>
+          <td>$420.00</td>
+        </tr>
+      </tbody>
+    </Table>
   </div>
 );
 export default Cart;

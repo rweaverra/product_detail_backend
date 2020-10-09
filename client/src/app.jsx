a7b85f485a2cce1ea2bf9e53b49e17cedd5f9aa9
Alison Sipos <alison.sipos@icloud.com>
2020-10-08 18:06:21 -0600

    Passed state around, imported some get requests, added cart state and setters.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -10,40 +11,40 @@ import Navigation from './components/navigation';
 import Search from './components/search';
 import Cart from './components/shoppingCart';
 import {
-  listProducts, // addToCart,
+  listProducts, addToCart,
   getAllStyles, getCart, getDefaultStyle, getInfo, getReviews,
 } from './lib/routes';
 
 const App = () => {
   const [styles, setStyles] = useState({});
   const [currentStyle, setCurrentStyle] = useState({});
-  const [productId, setId] = useState('1');
+  const [productId, setId] = useState(1);
   const [product, setProduct] = useState({});
   const [reviews, setReviews] = useState({});
   const [cart, setCart] = useState([]);
   const [productList, setProductList] = useState([]);
+  const [sessionId, setSessionId] = useState(8808);
 
   useEffect(() => {
-    getAllStyles((error, response) => {
+    getAllStyles(productId, (error, response) => {
       if (error) {
         return 'Could not get styles';
       }
       return setStyles(response);
     });
-    getInfo((error, response) => {
+    getInfo(productId, (error, response) => {
       if (error) {
         return 'Could not get products';
       }
-      setId(response.id);
       return setProduct(response);
     });
-    getReviews((error, response) => {
+    getReviews(productId, (error, response) => {
       if (error) {
         return 'Could not get reviews';
       }
       return setReviews(response.ratings);
     });
-    getDefaultStyle((error, response) => {
+    getDefaultStyle(productId, (error, response) => {
       if (error) {
         return 'Could not get default style';
       }
@@ -53,25 +54,33 @@ const App = () => {
       if (error) {
         return 'Could not get cart';
       }
-      // console.log('APP: get cart response', response);
+      console.log('APP: get cart response', response);
       return setCart(response);
+    });
+    addToCart(sessionId, productId, (error, response) => {
+      if (error) {
+        return 'Could not add to Cart';
+      }
+      console.log('add to cart success: ', response);
+      return getCart(sessionId);
     });
     listProducts((error, response) => {
       if (error) {
         return 'Could not get product list';
       }
-      // console.log('APP: get product list response', response);
       return setProductList(response);
     });
-  }, []);
+    setSessionId(8808);
+  }, [sessionId, productId]);
 
-  if (Object.keys(reviews).length === 0
+  if (
+    Object.keys(reviews).length === 0
+    || reviews === undefined
   || Object.keys(product).length === 0
+  || styles === undefined
+  || product === undefined
   || Object.keys(styles).length === 0
   || Object.keys(currentStyle).length === 0
-  || reviews === undefined
-  || product === undefined
-  || styles === undefined
   || currentStyle === undefined) {
     return (
       <div className="loading loading-img">
@@ -81,16 +90,21 @@ const App = () => {
       </div>
     );
   }
-
+  console.log('APP: cart ', cart);
   return (
     <Container>
       <div className="nav-wrap">
         <Row>
           <Col xs={10}>
-            <Navigation productId={productId} productList={productList} />
+            <Navigation
+              productList={productList}
+              styles={styles}
+            />
           </Col>
           <Col xs={2}>
-            <Search setId={setId} productId={productId} />
+            <Search
+              setId={setId}
+            />
           </Col>
         </Row>
         <TitleImage />
@@ -98,7 +112,11 @@ const App = () => {
           <Image className="nav-img-cart" src="https://icon-library.com/images/white-shopping-cart-icon/white-shopping-cart-icon-9.jpg" alt="cart" />
         </div>
         <div className="cart-container">
-          <Cart cart={cart} />
+          <Cart
+            cart={cart}
+            currentStyle={currentStyle}
+            product={product}
+          />
         </div>
       </div>
       <h2 className="sale">AUTUMN BACK TO SCHOOL SALE ~ SAVE 15% OFF YOUR FIRST ORDER ~ USE PROMO CODE: &apos;PROTO15&apos;</h2>
@@ -111,6 +129,10 @@ const App = () => {
           product={product}
           productId={productId}
           reviews={reviews}
+          sessionId={sessionId}
+          cart={cart}
+          setCart={setCart}
+          addToCart={addToCart}
         />
       </div>
       <div className="product-details-root">
